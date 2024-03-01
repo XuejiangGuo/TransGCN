abbb030cd5db14966464e98a7cfad616bcc47704
XuejiangGuo <100547857+XuejiangGuo@users.noreply.github.com>
2024-03-01 19:58:37 +0800

    Delete TransGCN directory

--- TransGCN/__init__.py
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author   : wangbing
-# @File        : __init__.py.py
-# @Time     : 5/11/2023 9:47 PM
-# @Emal     : wangbing587@163.com
-# @Desc     :
